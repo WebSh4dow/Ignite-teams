14636299e948271e3f2400739b50bf9fbc8cd089
WebSh4dow <phantomrising@protonmail.com>
2024-03-19 21:57:02 -0300

    ajuste identação

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,6 +2,7 @@ import { Header } from '@components/Header';
 import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+
 export function Groups() {
   return (
     <Container>
