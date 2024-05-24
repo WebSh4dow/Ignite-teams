29d9e79dbb4763ac8d1277d3a53261ab4b63f732
jarmison.paiva <jarmison6@gmail.com>
2024-05-24 09:06:00 -0300

    LocalStorage adicionado listagem e criacao

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,94 +1,148 @@
 import { Header } from "@components/Header";
-import { Container , Form, HeaderList, NumbersOfPlayers} from "./styles";
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
-import { FlatList } from "react-native";
-import { useState } from "react";
+import { Alert, FlatList, TextInput } from "react-native";
+import { useEffect, useRef, useState } from "react";
 import { PlayerCard } from "@components/PlayerCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
 import { useRoute } from "@react-navigation/native";
+import { AppError } from "@utils/AppError";
+import { PlayerAddByGroup } from "@storage/player/playerAddByGroup";
+import { playersGetByGroup } from "@storage/player/playersGetByGroup";
+import { playersGetByGroupAndTeam } from "@storage/player/playersGetByGroupAndTeam";
+import { PlayerStorageDTO } from "@storage/player/PlayerStorageDTO";
 
 type RouteParams = {
   group: string;
 }
 
 export function Players() {
+  const [newPlayerName, setNewPlayerName] = useState('');
 
   const [team, setTeam] = useState('Time A');
-  const [players, setPlayers] = useState(['Rodrigo', 'Marcos', 'Aline', 'Joabe', 'João', 'Felipe', 'Amanda', 'Bia'])
+  const [players, setPlayers] = useState<PlayerStorageDTO[]>([]);
 
   const route = useRoute();
-  const {group} = route.params as RouteParams;
+  const { group } = route.params as RouteParams;
 
-    return (
-        <Container>
-            <Header 
-                showBackButton
-            />
+  const newPlayerNameInputRef = useRef<TextInput>(null);
 
-            <Highlight
-                title={group}
-                subtitle="adicione a galera e separe os times"
-            />
-            <Form>
+  async function handleAddPlayer() {
+    if (newPlayerName.trim().length === 0) {
+      return Alert.alert('Nova pessoa', 'Informe o nome da pessoa para adicionar.')
+    }
 
-              <Input
-                placeholder="Nome da pessoa"
-                autoCorrect = {false}
-              />
+    const newPlayer = {
+      name: newPlayerName,
+      team,
+    }
+    try {
 
-              <ButtonIcon 
-                icon="add"
-              />
-            </Form>
+      await PlayerAddByGroup(newPlayer, group);
+      newPlayerNameInputRef.current?.blur();
 
-            <HeaderList>
+      setNewPlayerName('');
+      fetchPlayersByTeam();
 
-            <FlatList
-                data={['Time A', 'Time B']}
-                keyExtractor={item => item}
-                renderItem={({item}) => (
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert('Nova pessoa', error.message);
+
+      } else {
+        Alert.alert('Nova pessoa', 'Erro ao adicionar uma nova pessoa.')
+      }
+    }
+  }
+
+  async function fetchPlayersByTeam() {
+    try {
+      const playersByTeam = await playersGetByGroupAndTeam(group, team);
+      setPlayers(playersByTeam);
+    } catch (error) {
+      Alert.alert('Pessoas', 'Erro no carregamento de pessoas filtradas do time selecionado.');
+    }
+  }
+
+  useEffect(() => {
+    fetchPlayersByTeam();
+  }, [team]);
+
+  return (
+    <Container>
+      <Header
+        showBackButton
+      />
+
+      <Highlight
+        title={group}
+        subtitle="adicione a galera e separe os times"
+      />
+      <Form>
+
+        <Input
+          inputRef={newPlayerNameInputRef}
+          onChangeText={setNewPlayerName}
+          value={newPlayerName}
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+          onSubmitEditing={handleAddPlayer}
+          returnKeyType="done"
+        />
+
+        <ButtonIcon
+          icon="add"
+          onPress={handleAddPlayer}
+        />
+      </Form>
+
+      <HeaderList>
+
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
 
             <Filter
-                title={item}
-                isActive ={item === team}
-                onPress={()=> setTeam(item)}
-              />
-            )}
-              horizontal
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
             />
+          )}
+          horizontal
+        />
 
-            <NumbersOfPlayers>
-              {players.length}
-            </NumbersOfPlayers>
-          </HeaderList>
+        <NumbersOfPlayers>
+          {players.length}
+        </NumbersOfPlayers>
+      </HeaderList>
 
-          <FlatList
-            data={players}
-            keyExtractor={item => item}
-            renderItem={({item}) => (
+      <FlatList
+        data={players}
+        keyExtractor={item => item.name}
+        renderItem={({ item }) => (
 
-          <PlayerCard 
+          <PlayerCard
             onRemove={() => { }}
-            name={item}
+            name={item.name}
           />
         )}
-        ListEmptyComponent={() => (      
-          <ListEmpty 
+        ListEmptyComponent={() => (
+          <ListEmpty
             message='Não há pessoas nesse time.'
           />
         )}
-            showsVerticalScrollIndicator = {false}
-            contentContainerStyle = {[{paddingBottom: 100}, players.length === 0 && {flex: 1}]}
-          />
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[{ paddingBottom: 100 }, players.length === 0 && { flex: 1 }]}
+      />
 
-          <Button
-            title="Remover Turma"
-            type="SECUNDARY"
-          />
-        </Container>
-    );
+      <Button
+        title="Remover Turma"
+        type="SECUNDARY"
+      />
+    </Container>
+  );
 }
